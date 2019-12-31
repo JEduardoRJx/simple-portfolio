a6d7881fc384485029e65cc2ef6eb0137f3f3d55
Eduardo <jeduardorjx@gmail.com>
2019-12-30 21:54:46 -0700

    Import the Mountain image

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import { Mainbackgrnd } from '../components/mainbackgrnd/MainBackGrnd';
 import { Name } from '../components/name/Name';
+import { MtnCover } from '../components/mtncover/MtnCover';
 
 class App extends Component {
 
@@ -10,6 +11,7 @@ class App extends Component {
       <main>
         <Name />
         <Mainbackgrnd />
+        <MtnCover />
       </main>
     )
   }
