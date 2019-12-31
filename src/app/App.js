398f7a80d58cf9fd02fd273babf5b5e7fcc6cf37
Eduardo <jeduardorjx@gmail.com>
2019-12-30 20:46:25 -0700

    Change to a class component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Top } from '../components/top/Top'
 
 class App extends Component {
 
   render() {
     return (
       <main>
-        <h1>Hello</h1>
+        <Top />
       </main>
     )
   }
