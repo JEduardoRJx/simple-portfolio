6dba29deb314a94befde7ca1301fdb911e682514
Eduardo <jeduardorjx@gmail.com>
2019-12-30 21:45:27 -0700

    Component just for my name

---
+++ src/components/name/Name.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Name.scss';
+
+export const Name = () => {
+    return (
+      <section className='name-section'>
+        <h1 className='name'>
+          J. Eduardo Rodriguez
+        </h1>
+        <h2 className='name-software-developer'>
+          Software Developer
+        </h2>
+      </section>
+    )
+}
