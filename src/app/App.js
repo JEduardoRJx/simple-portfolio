db1facb77124c272d7baa3a04fd7923c029b6497
Eduardo <jeduardorjx@gmail.com>
2019-12-31 15:41:49 -0700

    Change name of the links component

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import { Mainbackgrnd } from '../components/mainbackgrnd/MainBackGrnd';
 import { Name } from '../components/name/Name';
 import { MtnCover } from '../components/mtncover/MtnCover';
 import { AboutMe } from '../components/aboutme/AboutMe';
-import { Links } from '../components/links/Links';
+import { NightSky } from '../components/nightsky/NightSky';
 import { Canyon } from '../components/canyon/Canyon';
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
         <Mainbackgrnd />
         <MtnCover />
         <AboutMe />
-        <Links />
+        <NightSky />
         <Canyon />
       </main>
     )
