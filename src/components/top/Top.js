52a3008e1de78ef964c2dd4ba7e26e33180e5b17
Eduardo <jeduardorjx@gmail.com>
2019-12-30 20:47:47 -0700

    All components will live here

---
+++ src/components/top/Top.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Top.scss';
+
+export const Top = () => {
+    return (
+      <section className='top-section'>
+        <img className='mountaing-img' src={require('../../assets/mountains_black_white.png')} />
+      </section>
+    )
+}
