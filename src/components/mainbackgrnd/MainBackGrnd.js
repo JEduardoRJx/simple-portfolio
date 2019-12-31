93fc81bc8ecfe626d58b3941116c92e99cf3fe7e
Eduardo <jeduardorjx@gmail.com>
2019-12-30 21:45:05 -0700

    Set a blank background just needed for color

---
+++ src/components/mainbackgrnd/MainBackGrnd.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './MainBackGrnd.scss';
+
+export const Mainbackgrnd = () => {
+    return (
+      <section className='mainbackgrnd-section'>
+      </section>
+    )
+}
