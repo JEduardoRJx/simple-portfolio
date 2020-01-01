15f7e400cb8df0379428591fd361406aed34f13c
Eduardo <jeduardorjx@gmail.com>
2019-12-31 18:56:27 -0700

    Create about me component

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -3,7 +3,7 @@ import './AboutMe.scss';
 
 export const AboutMe = () => {
     return (
-      <section className='about-me-section'>
+      <section className='aboutme-section'>
         <div className='about-me'>
           <img src={require('../../assets/me_bw.jpg')}/>
           {/* <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p> */}
