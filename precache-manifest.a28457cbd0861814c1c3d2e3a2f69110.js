c65b5a346df56422df8eff141096b59233516adf
Eduardo <jeduardorjx@gmail.com>
2020-01-02 14:29:21 -0700

    Updates

---
+++ precache-manifest.a28457cbd0861814c1c3d2e3a2f69110.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12795c4fc95e5ec07d2cd696f03f051f",
+    "url": "/simple-portfolio/index.html"
+  },
+  {
+    "revision": "d83c5cb581615bb68318",
+    "url": "/simple-portfolio/static/css/main.2384e620.chunk.css"
+  },
+  {
+    "revision": "502fd2b1fb6e14fa8784",
+    "url": "/simple-portfolio/static/js/2.86a7da6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-portfolio/static/js/2.86a7da6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d83c5cb581615bb68318",
+    "url": "/simple-portfolio/static/js/main.4597fff8.chunk.js"
+  },
+  {
+    "revision": "f4605e7e627dc3e199d8",
+    "url": "/simple-portfolio/static/js/runtime-main.5f5c7ac1.js"
+  },
+  {
+    "revision": "c581c62774abe52ceadd636d6f657ae6",
+    "url": "/simple-portfolio/static/media/canyon.c581c627.png"
+  },
+  {
+    "revision": "84839323a3749bd872e4b214441545f9",
+    "url": "/simple-portfolio/static/media/me_bw.84839323.jpg"
+  },
+  {
+    "revision": "782c8c4b44a454dd1321be3346c47639",
+    "url": "/simple-portfolio/static/media/mountains_black_white.782c8c4b.png"
+  },
+  {
+    "revision": "d11712f935f73e6aae797b1d5cdeb0fc",
+    "url": "/simple-portfolio/static/media/nightsky.d11712f9.jpg"
+  }
+]);
