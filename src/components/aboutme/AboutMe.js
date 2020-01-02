63fc7fbb11acc1e8baf9dddea1a5d4c44788e730
Eduardo <jeduardorjx@gmail.com>
2020-01-02 14:03:33 -0700

    Break the image and text to their own div containers

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -4,9 +4,11 @@ import './AboutMe.scss';
 export const AboutMe = () => {
     return (
       <section className='aboutme-section'>
-        <div className='about-me'>
+        <div className='aboutme-image'>
           <img src={require('../../assets/me_bw.jpg')}/>
-          {/* <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p> */}
+        </div>
+        <div className='aboutme-text'>
+         <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p>
         </div>
       </section>
     )
