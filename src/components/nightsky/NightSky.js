e19e9c6b0cd2e81413e6579297421fee346ba9b0
Eduardo <jeduardorjx@gmail.com>
2020-01-03 18:40:06 -0700

    Testing alignment on mobile devices

--- src/components/nightsky/NightSky.js
+++ src/components/nightsky/NightSky.js
@@ -5,12 +5,12 @@ export const NightSky = () => {
     return (
       <section className='nightsky-section'>
         <div className='aboutsite-wrapper'>
-          <div className='projects-wrapper'>
+          {/* <div className='projects-wrapper'> */}
             {/* <a>Harry Potter and the Sorting Game</a>
             <a>Movie Tracker</a>
             <a>What's New</a> */}
             {/* <h4>Site built on top of Facebook's Create React App</h4> */}
-          </div>
+          {/* </div> */}
           <div className='links-wrapper'>
             <a href="https://github.com/JEduardoRJx" target="_blank" rel="noopener noreferrer">
               <img className='icon' src={require('../../assets/github.png')} alt='github icon' />
