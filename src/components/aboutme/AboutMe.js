a476f2575f209fa00d8901a986b5599360b1cecb
Eduardo <jeduardorjx@gmail.com>
2020-01-03 17:20:51 -0700

    Add an alt tag to the profile picture

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -5,7 +5,7 @@ export const AboutMe = () => {
     return (
       <section className='aboutme-section'>
         <div className='aboutme-image'>
-          <img src={require('../../assets/me_bw.jpg')} alt='Image of J. Eduardo Rodriguez'/>
+          <img className='profile-picture' src={require('../../assets/me_bw.jpg')} alt='Image of J. Eduardo Rodriguez'/>
         </div>
         <div className='aboutme-text'>
          <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p>
