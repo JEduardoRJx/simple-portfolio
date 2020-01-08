8c6a69e0472d551c95de942976b97743aad39f93
Eduardo <jeduardorjx@gmail.com>
2020-01-08 13:31:14 -0700

    Change I like to I enjoy in about me

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -8,7 +8,7 @@ export const AboutMe = () => {
           <img className='profile-picture' src={require('../../assets/me_bw.jpg')} alt='Image of J. Eduardo Rodriguez'/>
         </div>
         <div className='aboutme-text'>
-         <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p>
+         <p> My emphasis is on front end technologies, resourceful problem solving, and collaboration. I enjoy working on a friendly, user focused team building software applications that are accessible and easy to use.</p>
         </div>
       </section>
     )
