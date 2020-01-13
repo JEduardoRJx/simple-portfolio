22b9a67fcfecf153cd9715377f7b83d8a2578268
Eduardo <jeduardorjx@gmail.com>
2020-01-13 12:10:42 -0700

    Add compressed png files to work with safari

--- src/components/nightsky/NightSky.js
+++ src/components/nightsky/NightSky.js
@@ -13,13 +13,13 @@ export const NightSky = () => {
           {/* </div> */}
           <div className='links-wrapper'>
             <a href="https://github.com/JEduardoRJx" target="_blank" rel="noopener noreferrer">
-              <img className='icon' src={require('../../assets/github.webp')} alt='github icon' />
+              <img className='icon' src={require('../../assets/github_opt.png')} alt='github icon' />
             </a>
             <a href="https://www.linkedin.com/in/jeduardorjx/" target="_blank" rel="noopener noreferrer">
-              <img className='icon' src={require('../../assets/linkedin.webp')} alt='linkedin icon' />
+              <img className='icon' src={require('../../assets/linkedin_opt.png')} alt='linkedin icon' />
             </a>
             <a href="mailto:jeduardorjx@gmail.com" target="_blank" rel="noopener noreferrer">
-              <img className='icon' src={require('../../assets/paper-plane.webp')} alt='email icon' />
+              <img className='icon' src={require('../../assets/paper-plane_opt.png')} alt='email icon' />
             </a>
           </div>
         </div>
